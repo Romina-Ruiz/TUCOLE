e47f1aabc1b09f7d67379cb54343bcb0ed909c9f
AriianaC <106198106+AriianaC@users.noreply.github.com>
2023-06-19 20:09:11 -0300

    Manager Materia h y cpp

---
+++ MANAGER/MateriaManager.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "MateriaManager.h"
+#include "Materia.h"
+#include "MateriaArchivo.h"
+
+using namespace std;
+
+
+int MateriaManager::Cargar()
+{
+
+    Materia aux;
+    MateriaArchivo ArchMateria;
+
+    char nombre, nombre_alumno,nombre_profesor;
+    int id, anio_curso, id_profesor;
+    bool agrego = false;
+//
+//    cout<<"NOMBRE : ";
+//    cin>>nombre;
+
+    cout<<"ID MATERIA: ";
+    cin>>id;
+
+    cout<<"ANIO DE CURSO: ";
+    cin>>anio_curso;
+
+//    cout<<"NOMBRE DEL ALUMNO: ";
+//    cin>>nombre_alumno;
+
+//    cout<<"NOMBRE DEL PROFESOR :";
+//    cin>>nombre_profesor;
+
+    cout<<"ID DEL PROFESOR";
+    cin>>id_profesor;
+
+   // aux.setNombreMateria(nombre);
+    aux.setId(id);
+    aux.setAnioCurso(anio_curso);
+   // aux.getNombreAlumno(nombre_alumno);
+   // aux.setNombreProfesor(nombre_profesor);
+    aux.setidProfesor(id_profesor);
+
+    agrego = ArchMateria.agregar(aux);
+
+    if (agrego == false)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+
+
+
+
+
---
+++ MANAGER/MateriaManager.h
@@ -0,0 +1,25 @@
+#ifndef MATERIAMANAGER_H
+#define MATERIAMANAGER_H
+#include "Materia.h"
+#include "MateriaArchivo.h"
+
+class MateriaManager
+{
+
+public:
+
+	int Cargar();
+//	void ListarTodos();
+//	void ListarXId();
+//	void Editar();
+//	void Eliminar();
+//	void Listar(Alumno alumno);
+//	int buscarDNI(int dni);
+//	int GenerarId();
+
+//	void HacerCopiaDeSeguridad();
+//	void RestaurarCopiaDeSeguridad();
+
+};
+
+#endif // MATERIAMANAGER_H
